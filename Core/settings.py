919593698abf0233cc6b4603126c2ec3ed6ebc34
Yousef Osama <yo5739240@gmail.com>
2026-02-07 20:27:11 +0200

    Cleanup: Remove database and cache from tracking

--- Core/settings.py
+++ Core/settings.py
@@ -88,9 +88,10 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'images'),
+    BASE_DIR / 'static',
 ]
 
 LOGIN_URL = 'login'
